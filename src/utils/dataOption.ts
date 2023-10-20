587320a2799b9a4bc33c76e4187427f26331c2b6
minji <321963741@naver.com>
2023-10-20 10:02:28 +0900

    Feat(dataOption) : add quiz setup options as constants

---
+++ src/utils/dataOption.ts
@@ -0,0 +1,44 @@
+const amountOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const categoryOptions = [
+  { value: "any", label: "Any Category" },
+  { value: "9", label: "General Knowledge" },
+  { value: "10", label: "Entertainment: Books" },
+  { value: "11", label: "Entertainment: Film" },
+  { value: "12", label: "Entertainment: Music" },
+  { value: "13", label: "Entertainment: Musicals & Theatres" },
+  { value: "14", label: "Entertainment: Television" },
+  { value: "15", label: "Entertainment: Video Games" },
+  { value: "16", label: "Entertainment: Board Games" },
+  { value: "17", label: "Science & Nature" },
+  { value: "18", label: "Science: Computers" },
+  { value: "19", label: "Science: Mathematics" },
+  { value: "20", label: "Mythology" },
+  { value: "21", label: "Sports" },
+  { value: "22", label: "Geography" },
+  { value: "23", label: "History" },
+  { value: "24", label: "Politics" },
+  { value: "25", label: "Art" },
+  { value: "26", label: "Celebrities" },
+  { value: "27", label: "Animals" },
+  { value: "28", label: "Vehicles" },
+  { value: "29", label: "Entertainment: Comics" },
+  { value: "30", label: "Science: Gadgets" },
+  { value: "31", label: "Entertainment: Japanese Anime & Manga" },
+  { value: "32", label: "Entertainment: Cartoon & Animations" },
+];
+
+const difficultyOptions = [
+  { value: "any", label: "Any Difficulty" },
+  { value: "easy", label: "Easy" },
+  { value: "medium", label: "Medium" },
+  { value: "hard", label: "Hard" },
+];
+
+const typeOptions = [
+  { value: "any", label: "Any Type" },
+  { value: "multiple", label: "Multiple Choice" },
+  { value: "boolean", label: "True / False" },
+];
+
+export { amountOptions, categoryOptions, difficultyOptions, typeOptions };
