7d63986f5e0e92c096ad62b784eb5709fc096292
minji <321963741@naver.com>
2023-10-20 23:11:57 +0900

    Refactor(Redicrect) : rename text

--- src/components/Redirect.tsx
+++ src/components/Redirect.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Redirect = () => {
   return (
     <>
-      <div>퀴즈가 중간에 종료되었습니다. </div>
+      <div>퀴즈가 종료 되었습니다. </div>
       <Link to="/">홈으로 돌아가기</Link>
     </>
   );
