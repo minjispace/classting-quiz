e50693e5867de19e201a922bf19cdff9811ee54e
minji <321963741@naver.com>
2023-10-20 23:09:31 +0900

    Feat(context) :  Added logic for start and end times, tracking correct and incorrect counts, and calculating elapsed time

--- src/context/quizContext.tsx
+++ src/context/quizContext.tsx
@@ -1,11 +1,74 @@
-import { ReactNode, createContext, useContext } from "react";
+import { ReactNode, createContext, useContext, useState } from "react";
 
-interface QuizContextProps {}
+// 결과 정보를 담는 타입 정의
+type QuizResult = {
+  startTime: number | null;
+  correctAnswers: number;
+  wrongAnswers: number;
+  usedTime: number;
+};
 
-const QuizContext = createContext<QuizContextProps | undefined>(undefined);
+type QuizContextType = {
+  quizResult: QuizResult;
+  startQuizTime: () => void;
+  endQuizTime: () => void;
+  updateAnswerCounts: (isCorrect: boolean) => void;
+};
+
+const QuizContext = createContext<QuizContextType | undefined>(undefined);
 
 export function QuizProvider({ children }: { children: ReactNode }) {
-  return <QuizContext.Provider value={{}}>{children}</QuizContext.Provider>;
+  const [quizResult, setQuizResult] = useState<QuizResult>({
+    startTime: null,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+    usedTime: 0,
+  });
+
+  // 퀴즈 시작 시간 설정
+  const startQuizTime = () => {
+    setQuizResult({
+      ...quizResult,
+      startTime: Date.now(),
+      correctAnswers: 0,
+      wrongAnswers: 0,
+    });
+  };
+
+  // 퀴즈 종료 시간 설정
+  const endQuizTime = () => {
+    if (quizResult.startTime) {
+      const endTime = Date.now();
+      const usedTime = endTime - quizResult.startTime;
+      setQuizResult({
+        ...quizResult,
+        usedTime,
+      });
+    }
+  };
+
+  // 정답 오답 업데이트 함수
+  const updateAnswerCounts = (isCorrect: boolean) => {
+    if (isCorrect) {
+      setQuizResult({
+        ...quizResult,
+        correctAnswers: quizResult.correctAnswers + 1, // 정답일 때 정답 갯수 증가
+      });
+    } else {
+      setQuizResult({
+        ...quizResult,
+        wrongAnswers: quizResult.wrongAnswers + 1, // 오답일 때 오답 갯수 증가
+      });
+    }
+  };
+
+  return (
+    <QuizContext.Provider
+      value={{ quizResult, startQuizTime, endQuizTime, updateAnswerCounts }}
+    >
+      {children}
+    </QuizContext.Provider>
+  );
 }
 
 export function useQuizContext() {
