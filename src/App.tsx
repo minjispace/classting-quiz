76ea44e34cf61ae073e09fff4df4305bba352087
minji <321963741@naver.com>
2023-10-20 12:08:23 +0900

    Feat(router) : configured routing using React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import SetupForm from "./components/SetupForm";
+import Quiz from "./components/Quiz";
 
 function App() {
-  return <SetupForm />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SetupForm />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
