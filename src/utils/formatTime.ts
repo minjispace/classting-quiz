cd41c6ff255d5564740521767623f4fb5a589641
minji <321963741@naver.com>
2023-10-20 23:10:52 +0900

    Feat(utils) : Added a time formatting function

---
+++ src/utils/formatTime.ts
@@ -0,0 +1,7 @@
+// 시간 포맷팅 함수 (예: 밀리초를 분:초 형식으로 변환)
+export const formatTime = (milliseconds: number) => {
+  const seconds = Math.floor(milliseconds / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes}분${remainingSeconds}초`;
+};
