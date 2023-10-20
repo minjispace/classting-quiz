eeca9b943612a88b8819c1d9f0ab8072b67734c9
minji <321963741@naver.com>
2023-10-20 10:48:07 +0900

    Feat(quizAPI): Skip sending params when data type is any in quiz API

--- src/api/quizAPI.ts
+++ src/api/quizAPI.ts
@@ -1,16 +1,28 @@
 import axios from "axios";
 import { QuizType } from "../components/SetupForm";
 
-const BASE_URL = "https://opentdb.com/api_config.php";
+const BASE_URL = "https://opentdb.com/api.php";
 
 const getQuizData = async (quiz: QuizType) => {
   const { amount, category, difficulty, type } = quiz;
-  const params = {
+
+  const params: Record<string, any> = {
     amount,
-    category,
-    difficulty,
-    type,
   };
+  // type이 any가 아닐경우만 params 추가
+  if (type !== "any") {
+    params["type"] = type;
+  }
+
+  // category any가 아닐경우만 params 추가
+  if (category !== "any") {
+    params["category"] = category;
+  }
+
+  // difficulty any가 아닐경우만 params 추가
+  if (difficulty !== "any") {
+    params["difficulty"] = difficulty;
+  }
 
   const response = await axios.get(BASE_URL, {
     params,
