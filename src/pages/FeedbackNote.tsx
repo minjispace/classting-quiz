914c5e7303f7c95e6bff6b85cbd492f2b6e1cffa
minji <321963741@naver.com>
2023-10-21 15:01:19 +0900

    Feat(FeedbackNote) : initialize FeedbackNote component UI

--- src/pages/FeedbackNote.tsx
+++ src/pages/FeedbackNote.tsx
@@ -1,5 +1,34 @@
+import { Link } from "react-router-dom";
+import { convertTimestampToDate } from "../utils/formatTime";
+import { useWrongAnswerContext } from "../context/\bwrongAnswerContext";
+
 const FeedbackNote = () => {
-  return <div>feedback note</div>;
+  const { getWrongAnswers } = useWrongAnswerContext();
+
+  const wrongAnswerData = getWrongAnswers();
+
+  return (
+    <div>
+      <h2>오답노트</h2>
+      {wrongAnswerData?.map((item, index) => {
+        const { category, question, correctAnswer, date } = item;
+
+        return (
+          <ul style={{ backgroundColor: "gray" }} key={index}>
+            <h3>문제 카테고리 : {category}</h3>
+            <article>
+              <h3>문제 : {question}</h3>
+              <h2>정답 : {correctAnswer}</h2>
+              <p>{convertTimestampToDate(date)}</p>
+            </article>
+          </ul>
+        );
+      })}
+      <Link to="/">
+        <button>다른 문제 풀러가기</button>
+      </Link>
+    </div>
+  );
 };
 
 export default FeedbackNote;
