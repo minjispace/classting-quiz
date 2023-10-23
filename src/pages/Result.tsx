9c715763cca4b121b0e2402e79e3e56c6ca1d338
minji <321963741@naver.com>
2023-10-23 10:36:19 +0900

    Feat(Result) : Implement Quiz Result Logic

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,48 +1,54 @@
 import { Link } from "react-router-dom";
 import { useQuizContext } from "../context/quizContext";
 import { formatTime } from "../utils/formatTime";
-import Redirect from "../components/Redirect";
+import RedirectHome from "../components/RedirectHome";
 import ResultChart from "../components/ResultChart";
+import { getDataFromLocalStorage } from "../utils/localStorage.quizResult";
 
 const Result = () => {
-  const { quizResult } = useQuizContext();
+  const { quizStartTime } = useQuizContext();
 
-  // 정답, 오답 및 총 시간 가져오기
-  const { correctAnswers, wrongAnswers, startTime } = quizResult;
+  // 해당 quiz data localstorage에서 가져오기
+  const data = getDataFromLocalStorage()?.find(
+    (item) => item.startTime === quizStartTime,
+  );
 
-  // 퀴즈를 마칠 때까지 소요된 시간 계산
-  const endTime = Date.now();
-  const elapsedMilliseconds = startTime ? endTime - startTime : 0;
-
-  // // redirect page
-  if (!startTime) return <Redirect />;
+  // 데이터가 없다면 홈으로
+  if (!data || !data.startTime) {
+    return <RedirectHome />;
+  }
 
   return (
     <main>
-      {/* description */}
+      {/* quiz result description */}
       <h1>결과</h1>
-      <p>퀴즈를 마칠 때까지 소요된 시간: {formatTime(elapsedMilliseconds)}</p>
-      <p>정답 개수: {correctAnswers}</p>
-      <p>오답 수: {wrongAnswers}</p>
+      <p>
+        퀴즈를 마칠 때까지 소요된 시간:{" "}
+        {formatTime(data.endTime - data.startTime)}
+      </p>
+      <p>정답 개수: {data.correct}</p>
+      <p>오답 수: {data.wrong}</p>
+      <div>총 문제수 {data.correct + data.wrong}</div>
       <p>
         정답 비율:{" "}
-        {((correctAnswers / (correctAnswers + wrongAnswers)) * 100).toFixed(2)}%
+        {((data.correct / (data.correct + data.wrong)) * 100).toFixed(2)}%
       </p>
 
       {/* chart */}
-      <ResultChart
-        correctAnswers={correctAnswers}
-        wrongAnswers={wrongAnswers}
-      />
+      <ResultChart correctAnswers={data.correct} wrongAnswers={data.wrong} />
+
+      {/* feedback note button */}
+      {data?.wrongResult?.length === 0 ? (
+        <div>만점입니다. 축하합니다!!</div>
+      ) : (
+        <Link to={`/feed-back/${data.startTime}`}>
+          <button>오답노트</button>
+        </Link>
+      )}
 
       {/* home button */}
       <Link to="/">
         <button>다른 문제 풀러가기</button>
-      </Link>
-
-      {/* feedback note button */}
-      <Link to="/feed-back">
-        <button>오답노트</button>
       </Link>
     </main>
   );
