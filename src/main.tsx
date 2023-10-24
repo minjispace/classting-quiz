58680cb774e9675151dd16908d53282ee4885393
minji <321963741@naver.com>
2023-10-24 16:17:24 +0900

    Refactor : update code for testing in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { QuizProvider } from "./context/quizContext.tsx";
+import { createRoot } from "react-dom/client";
 
 // Create a client
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <QuizProvider>
-        <App />
-      </QuizProvider>
-    </QueryClientProvider>
-  </React.StrictMode>,
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <QuizProvider>
+          <App />
+        </QuizProvider>
+      </QueryClientProvider>
+    </React.StrictMode>,
+  );
+}
